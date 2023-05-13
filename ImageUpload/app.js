bbf7e951fffc94977ba1ec69c477237bce9f09fd
Vaibhav Gawali <vaibhav@123@gmail.com>
2023-05-13 12:58:56 +0530

    Uploading image with express-fileupload and body-parser

---
+++ ImageUpload/app.js
@@ -0,0 +1,33 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const fileUpload=require("express-fileupload");
+
+const app=express();
+const port = process.env.PORT || 9877;
+
+// static files
+app.use(express.static(__dirname+'/public'))
+app.set('views','./src/views')
+app.set('view engine','ejs')
+
+// middleware
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(fileUpload())
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.post('/upload',async (req,res)=>{
+    console.log(req.files)
+    console.log(req.body)
+    const imageFile=req.files.uimage;
+    await imageFile.mv(`${__dirname}/public/images/${imageFile.name}`)
+    res.render('display',{title:req.body.iname,image:`${imageFile.name}`})
+})
+
+app.listen(port,(err)=>{
+    if(err) throw err
+    console.log(`listening on port ${port}`)
+})
